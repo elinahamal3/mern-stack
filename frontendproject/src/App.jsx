c46311468c319ab41bf1c74d2a1062b668474505
elinahamal3 <elinahamal3@gmail.com>
2025-09-03 16:43:33 +0545

    hh

--- frontendproject/src/App.jsx
+++ frontendproject/src/App.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./Pages/Home/Home";
 import SingleBook from "./Pages/singleBook/singleBook";
 import AddBook from "./Pages/addBook/addBook";
-import EditBook from "./Pages/editBook/editBook";
+import EditBook from "./Pages/editBook/EditBook";
 
 const App = () => {
   return (
